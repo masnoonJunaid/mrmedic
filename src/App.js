3a29e791196f29d28557dd0cca12652c4b9cd80b
Masnoon Junaid <masnoon20@gmail.com>
2021-07-02 20:16:04 +0530

    Initial structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>
+      mrmedic University project
+    </h1>
+    <h1>
+      Final year Computer Science and Engineering project
+    </h1>
+    </div>
+  );
+}
+
+export default App;
