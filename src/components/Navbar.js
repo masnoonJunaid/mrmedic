43727fb7d10ab14ce09722e97b203311f2baf988
masnoonJunaid <masnoon20@gmail.com>
2021-08-27 09:54:08 +0530

    name change

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import '../App.css';
+import styled from "styled-components"
+function Navbar(){
+  return (
+    <NavStyle>
+    <div>
+    <h1>
+    MedApp
+    </h1>
+    {/*
+      <li className="nav-list">Hospitals</li>
+      <li className="nav-list">Vaccinations</li>
+      <li className="nav-list">Ambulance</li>
+      */}
+    </div>
+    </NavStyle>
+  )
+}
+
+
+export const NavStyle = styled.div`
+
+
+h1{
+  font-family: 'Uchen', serif;
+  color:ghostwhite;
+  margin-left:20vw
+}
+
+
+`
+
+
+
+export default  Navbar;
